d76b2eb4326005f5208b3154ac929309c1bcf519
Dan Mulvihill <dmulvihill3@gmail.com>
2017-11-29 16:20:02 -0500

    Fixed bug and add headings/description

--- index.php
+++ index.php
@@ -3,19 +3,22 @@
 <title>PHP to JS Wormhole</title>
 
 <article>
+
+<h2>PHP to JS Wormhole</h2>
+<p>This file takes a PHP associative array and converts it to a JavaScript object literal
+by through JSON encoding.</p> 
 <div id="php">
-<?php echo "PHP Associative Array: <br>";
+<?php echo "<h3>PHP Associative Array: </h3>";
 $PHP_to_JS = ["Name"=>"Pete", "Pos"=>"Web developer", "Skills"=>["JS", "PHP", "WP" ] ];
 print_r($PHP_to_JS);
 ?>
 </div>
-<br>
-After JSON enoding:<br>
+<h3>After JSON encoding:</h3>
 <div id="wormhole">
 <?php echo json_encode($PHP_to_JS); ?> 
 </div>
-<br>
-Retrieved with JS:  
+<h3>
+Retrieved with JS:  </h3>
 <div id="PHPout">
 
 </div>
@@ -24,11 +27,7 @@ let P2J = document.getElementById("wormhole").innerText;
 document.getElementById("PHPout").innerHTML = P2J;
 </script>
 
- <h1><?php the_title(); ?></h1>
- <div><?php the_post_thumbnail("medium"); ?></div>
- <p><?php echo get_the_content(); ?></p>
- 
-	<?php endwhile; endif; ?>
+
  </article>
  
  
